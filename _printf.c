f9cfdf523948bbb52e66690c1eb76600dd98bcdc
Yosri-ctrl <yosribouabid@gmail.com>
2020-03-17 21:41:42 +0100

    0

--- _printf.c
+++ _printf.c
@@ -9,10 +9,7 @@
 int _printf(const char *format, ...)
 {
 class new[] = {
-{"c", p_char},
-{"s", p_string},
-{"d", p_decimal},
-{"i", p_integer}
+{"c", p_char}, {"s", p_string}, {"d", p_decimal}, {"i", p_integer}
 };
 
 va_list argument;
@@ -25,41 +22,29 @@ va_start(argument, format);
 if (!format || argument == NULL)
 return (-1);
 
-/*format [0, ...]*/
 for (i = 0; i < len; i++)
 {
-    /*new[0, ...].character*/
-    for (j = 0; j < 2; j++)
-    {
-        if (format[i] == '%' && format[i + 1] == '\0')
-        return (-1);
-        /* if format is just a string */
-        if (!format && argument == NULL)
-        {
-            for (x = 0; x < len; x++)
-            _putchar(format[x]);
-            return (1);
-        }
-        /* for "%%" */ 
-        if (format[i] == '%' && format[i + 1] == '%')
-        {
-            _putchar('%');
-            i++;
-        }
-        /*if the character from format match character from the array*/
-        if (format[i] == '%' && format[i + 1] == *new[j].character)
-        {
-            new[j].function(argument);
-            if (i < len)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-            count++;
-        }
-    }
+for (j = 0; j < 2; j++)
+{
+if (format[i] == '%' && format[i + 1] == '\0')
+return (-1);
+if (format[i] == '%' && format[i + 1] == '%')
+{
+_putchar('%');
+i++;
 }
-printf("%d", count);
+if (format[i] == '%' && format[i + 1] == *new[j].character)
+{
+new[j].function(argument);
+if (i < len)
+{
+_putchar(',');
+_putchar(' ');
+}
+count++;
+}
+}
+}
 va_end(argument);
 return (count);
 }
