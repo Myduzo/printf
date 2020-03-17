476fb89a9d882b4b87c553e3658f9a6433f576d0
Yosri-ctrl <yosribouabid@gmail.com>
2020-03-17 22:58:42 +0100

    0

--- sub_printf.c
+++ sub_printf.c
@@ -1,30 +1,27 @@
 #include "holberton.h"
 #include <stdarg.h>
 /**
- * 
- * 
- * 
+ * sub_print - next step
+ * @c : the character to print
+ * @a : the argument to pass
  */
 void sub_print(char c, va_list a)
 {
-    class new[] = {
-    {"c", p_char},
-    {"s", p_string},
-    {"d", p_decimal},
-    {"i", p_integer}
-    };
-    int i;
+class new[] = {
+{"c", p_char},
+{"s", p_string},
+{"d", p_decimal},
+{"i", p_integer}
+};
+int i;
 
-    for (i = 0; i < 5; i++)
-    {
-        if (c == *new[i].character)
-        {
-            new[i].function(a);
-        }
-        if (c == '%')
-        _putchar('%');
-
-        
-    }
- 
+for (i = 0; i < 5; i++)
+{
+if (c == *new[i].character)
+{
+new[i].function(a);
+}
+if (c == '%')
+_putchar('%');
+}
 }
