de1e833d20dd92429b7d145a41194cfcb11dd8a8
Myduzo <sahli.youssef@outlook.com>
2020-03-18 15:46:23 +0100

    0

--- _printf.c
+++ _printf.c
@@ -14,21 +14,17 @@ va_list arg;
 int count = 0;
 int len, i, j;
 
-for (len = 0; format[len] != '\0'; len++)
-;
 va_start(arg, format);
 if (!format || arg == NULL)
 return (-1);
 
-while (!format)
+while (*format)
 {
-if (*format == '%')
+if (*format == '%' && )
+{
 sub_printf(*format++, arg)
-
-if (*format++)
-{
-_putchar(*format);
 }
+_putchar(*format);
 }
 va_end(arg);
 return (count);
